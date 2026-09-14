ae7b38d66912bf41142ef7427ee8feb0a17c2cdf
Omercanbasboga <94013047+Omercanbasboga@users.noreply.github.com>
2026-09-14 11:08:03 +0300

    Add files via upload

---
+++ src/main/java/dev/omercanbasboga/hollowmereliveops/HollowmereLiveopsApplication.java
@@ -0,0 +1,12 @@
+package dev.omercanbasboga.hollowmereliveops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollowmereLiveopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HollowmereLiveopsApplication.class, args);
+    }
+}
